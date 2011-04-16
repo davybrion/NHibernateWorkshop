f9dc595bb5eca99a7fb1fed33c8a67c77b5407e5
Davy Brion <ralinx@davybrion.com>
2011-04-16 13:32:05 +0200

    example of advantage of detached criteria

---
+++ NHibernateWorkshop/Querying/Criteria/DetachedCriteriaVsICriteriaFixture.cs
@@ -0,0 +1,23 @@
+using NHibernate.Criterion;
+using Northwind.Entities;
+using NUnit.Framework;
+
+namespace NHibernateWorkshop.Querying.Criteria
+{
+    [TestFixture]
+    public class DetachedCriteriaVsICriteriaFixture : Fixture
+    {
+        [Test]
+        public void detached_criteria_can_be_created_outside_of_session_scope()
+        {
+            var query = DetachedCriteria.For<Customer>()
+                .Add(Restrictions.Gt("DiscountPercentage", 0.2d));
+
+            using (var session = CreateSession())
+            {
+                Logger.Info("the query has already been created but we can only execute it through an open session");
+                var results = query.GetExecutableCriteria(session).List();
+            }
+        }
+    }
+}
