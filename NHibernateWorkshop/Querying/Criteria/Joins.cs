5db2ccf1c8d9f2a2c3cd7abb7dcb24f40ba6fe40
Davy Brion <ralinx@davybrion.com>
2011-04-25 11:03:51 +0200

    join examples for criteria

---
+++ NHibernateWorkshop/Querying/Criteria/Joins.cs
@@ -0,0 +1,152 @@
+using NHibernate;
+using NHibernate.Criterion;
+using NHibernate.SqlCommand;
+using NHibernate.Transform;
+using Northwind.Builders;
+using Northwind.Entities;
+using NUnit.Framework;
+using Order = Northwind.Entities.Order;
+
+namespace NHibernateWorkshop.Querying.Criteria
+{
+    [TestFixture]
+    public class Joins : AutoRollbackFixture
+    {
+        [Test]
+        public void join_on_a_many_to_one_with_implicit_inner_join()
+        {
+            var ordersWithCustomers = Session.CreateCriteria<Order>()
+                .SetFetchMode("Customer", FetchMode.Join) // implicit inner join because Customer is a required property
+                .List<Order>();
+
+            // IsInitialized would return false if Customer was a proxy
+            ordersWithCustomers.Each(o => Assert.IsTrue(NHibernateUtil.IsInitialized(o.Customer)));
+        }
+
+        [Test]
+        public void join_on_a_many_to_one_with_implicit_outer_join()
+        {
+            var employeeWithoutManager = new EmployeeBuilder().Build();
+            Session.Save(employeeWithoutManager);
+
+            var employeeWithManager = new EmployeeBuilder().WithManager(employeeWithoutManager).Build();
+            Session.Save(employeeWithManager);
+
+            FlushAndClear();
+
+            var employeesWithOrWithoutManagers = Session.CreateCriteria<Employee>()
+                .SetFetchMode("Manager", FetchMode.Join) // implicit outer join because Manager is an optional property
+                .List<Employee>();
+
+            Assert.IsTrue(employeesWithOrWithoutManagers.Contains(employeeWithoutManager));
+            Assert.IsTrue(employeesWithOrWithoutManagers.Contains(employeeWithManager));
+            Assert.IsNull(employeeWithoutManager.Manager);
+            Assert.IsTrue(NHibernateUtil.IsInitialized(employeeWithManager.Manager));
+        }
+
+        [Test]
+        public void join_on_a_many_to_one_with_explicit_inner_join()
+        {
+            var employeeWithoutManager = new EmployeeBuilder().Build();
+            Session.Save(employeeWithoutManager);
+
+            var employeeWithManager = new EmployeeBuilder().WithManager(employeeWithoutManager).Build();
+            Session.Save(employeeWithManager);
+
+            FlushAndClear();
+
+            var employeesWithManagers = Session.CreateCriteria<Employee>()
+                .CreateCriteria("Manager", JoinType.InnerJoin)
+                .List<Employee>();
+
+            Assert.IsFalse(employeesWithManagers.Contains(employeeWithoutManager));
+            Assert.IsTrue(employeesWithManagers.Contains(employeeWithManager));
+            employeesWithManagers.Each(e => Assert.IsTrue(NHibernateUtil.IsInitialized(e.Manager)));
+        }
+
+        [Test]
+        public void join_on_a_many_to_one_and_one_of_its_many_to_ones()
+        {
+            var ordersWithEmployeesAndTheirManagers = Session.CreateCriteria<Order>()
+                .CreateCriteria("Employee", "e", JoinType.InnerJoin)
+                .CreateCriteria("e.Manager", JoinType.LeftOuterJoin) // you have to pick the type of join you want here
+                .List<Order>();
+
+            ordersWithEmployeesAndTheirManagers.Each(o =>
+            {
+                Assert.IsTrue(NHibernateUtil.IsInitialized(o.Employee));
+
+                if (o.Employee.Manager != null)
+                {
+                    Assert.IsTrue(NHibernateUtil.IsInitialized(o.Employee.Manager));
+                }    
+            });
+        }
+
+        [Test]
+        public void join_on_two_many_to_ones_with_implicit_inner_joins()
+        {
+            var ordersWithCustomersAndEmployees = Session.CreateCriteria<Order>()
+                .SetFetchMode("Employee", FetchMode.Join)
+                .SetFetchMode("Customer", FetchMode.Join)
+                .List<Order>();
+
+            ordersWithCustomersAndEmployees.Each(o => Assert.IsTrue(NHibernateUtil.IsInitialized(o.Employee)));
+            ordersWithCustomersAndEmployees.Each(o => Assert.IsTrue(NHibernateUtil.IsInitialized(o.Customer)));
+        }
+
+        [Test]
+        public void join_on_one_to_many_without_distinct_result_transformer()
+        {
+            var ordersWithItems = Session.CreateCriteria<Order>()
+                .SetFetchMode("Items", FetchMode.Join)
+                .List<Order>();
+
+            var orderCount = Session.CreateCriteria<Order>()
+                .SetProjection(Projections.RowCount())
+                .UniqueResult<int>();
+
+            var orderItemCount = Session.CreateCriteria<OrderItem>()
+                .SetProjection(Projections.RowCount())
+                .UniqueResult<int>();
+
+            // oops... ordersWithItems contains an element for each OrderItem
+            Assert.AreNotEqual(orderCount, ordersWithItems.Count);
+            Assert.AreEqual(orderItemCount, ordersWithItems.Count);
+        }
+
+        [Test]
+        public void join_on_one_to_many_with_distinct_result_transformer()
+        {
+            var ordersWithItems = Session.CreateCriteria<Order>()
+                .SetFetchMode("Items", FetchMode.Join)
+                .SetResultTransformer(new DistinctRootEntityResultTransformer())
+                .List<Order>();
+
+            var orderCount = Session.CreateCriteria<Order>()
+                .SetProjection(Projections.RowCount())
+                .UniqueResult<int>();
+
+            Assert.AreEqual(orderCount, ordersWithItems.Count);
+            ordersWithItems.Each(o => Assert.IsTrue(NHibernateUtil.IsInitialized(ReflectionHelper.GetPrivateFieldValue(o, "_items"))));
+        }
+
+        [Test]
+        public void join_on_one_to_many_and_one_of_its_many_to_ones()
+        {
+            var ordersWithItemsAndProducts = Session.CreateCriteria<Order>()
+                .CreateCriteria("Items", "items", JoinType.LeftOuterJoin) 
+                .CreateCriteria("items.Product", JoinType.InnerJoin)
+                .SetResultTransformer(new DistinctRootEntityResultTransformer())
+                .List<Order>();
+
+            ordersWithItemsAndProducts.Each(o =>
+            {
+                // note: using an inner join would make this assertion fail for some unclear reason... it would
+                // also trigger a select when trying to access the Items collection
+                Assert.IsTrue(NHibernateUtil.IsInitialized(ReflectionHelper.GetPrivateFieldValue(o, "_items")));
+                o.Items.Each(i => Assert.IsTrue(NHibernateUtil.IsInitialized(i.Product)));
+            });
+        }
+    }
+}
---
+++ NHibernateWorkshop/ReflectionHelper.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+
+namespace NHibernateWorkshop
+{
+    public static class ReflectionHelper
+    {
+        public static object GetPrivateFieldValue(object theObject, string fieldName)
+        {
+            var type = theObject.GetType();
+            return type.GetField(fieldName, BindingFlags.Instance | BindingFlags.NonPublic).GetValue(theObject);
+        }
+
+    }
+}
