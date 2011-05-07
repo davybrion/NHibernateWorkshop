6867759f2197db8ab6cdf3099793fbe9504b8993
Davy Brion <ralinx@davybrion.com>
2011-05-07 16:54:08 +0200

    group by example for QueryOver

---
+++ NHibernateWorkshop/Querying/QueryOver/GroupBy.cs
@@ -0,0 +1,65 @@
+using System.Collections.Generic;
+using System.Linq;
+using NHibernate.Criterion;
+using NHibernate.Transform;
+using Northwind.Dtos;
+using Northwind.Entities;
+using NUnit.Framework;
+
+namespace NHibernateWorkshop.Querying.QueryOver
+{
+    [TestFixture]
+    public class GroupBy : AutoRollbackFixture
+    {
+        private IList<OrderItem> _orderItems;
+
+        protected override void AfterSetUp()
+        {
+            _orderItems = Session.QueryOver<OrderItem>().List();
+        }
+
+        [Test]
+        public void retrieve_product_sales_summaries()
+        {
+            Product product = null;
+
+            var productSalesSummaries = Session.QueryOver<OrderItem>()
+                .JoinAlias(o => o.Product, () => product)
+                .Select(Projections.ProjectionList()
+                    .Add(Projections.Group(() => product.Id).As("ProductId"))
+                    .Add(Projections.Group(() => product.Name).As("ProductName"))
+                    .Add(Projections.Sum<OrderItem>(o => o.Quantity).As("ItemsSold")))
+                .TransformUsing(new AliasToBeanResultTransformer(typeof(ProductSalesSummary)))
+                .List<ProductSalesSummary>();
+
+            productSalesSummaries.Each(p => Assert.AreEqual(GetSalesCountForProductWithId(p.ProductId), p.ItemsSold));
+        }
+
+        [Test]
+        public void retrieve_product_sales_summaries_of_products_that_sold_at_least_100_items()
+        {
+            Product product = null;
+
+            var productSalesSummaries = Session.QueryOver<OrderItem>()
+                .JoinAlias(o => o.Product, () => product)
+                .Select(Projections.ProjectionList()
+                    .Add(Projections.Group(() => product.Id).As("ProductId"))
+                    .Add(Projections.Group(() => product.Name).As("ProductName"))
+                    .Add(Projections.Sum<OrderItem>(o => o.Quantity).As("ItemsSold")))
+                .Where(Restrictions.Gt(Projections.Sum<OrderItem>(o => o.Quantity), 100))
+                .TransformUsing(new AliasToBeanResultTransformer(typeof(ProductSalesSummary)))
+                .List<ProductSalesSummary>();
+
+            productSalesSummaries.Each(p =>
+            {
+                Assert.AreEqual(GetSalesCountForProductWithId(p.ProductId), p.ItemsSold);
+                Assert.Greater(p.ItemsSold, 100);
+            });
+        }
+
+        private long GetSalesCountForProductWithId(int productId)
+        {
+            return _orderItems.Where(o => o.Product.Id == productId).Sum(o => o.Quantity);
+        }
+    }
+}
