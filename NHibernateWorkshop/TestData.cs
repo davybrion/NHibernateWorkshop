30b93c35eca6f3b1fa291c73efb3a5839128b378
Mark Meyers <kilfour@gmail.com>
2011-05-10 22:16:21 +0200

    upgraded quickgenerate to 0.4.5

--- NHibernateWorkshop/TestData.cs
+++ NHibernateWorkshop/TestData.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using NHibernate;
 using Northwind.Components;
@@ -10,56 +11,57 @@ namespace NHibernateWorkshop
 {
     public static class TestData
     {
-        private static DomainGenerator WithAddress(DomainGenerator generator)
+        public static class Using
         {
-            return generator
-                .Component<Address>()
-                .With<Address>(options => options.For(address => address.Street, new StringGenerator(1, 100)))
-                .With<Address>(options => options.For(address => address.City, new StringGenerator(1, 100)))
-                .With<Address>(options => options.For(address => address.Country, new StringGenerator(1, 100)));
+            public static DomainGenerator TheseConventions()
+            {
+                return
+                    new DomainGenerator()
+                        .With<Entity<int>>(options => options.Ignore(entity => entity.Id))
+                        .With<Entity<Guid>>(options => options.Ignore(entity => entity.Id))
+                        .Component<Address>()
+                        .With<Address>(options => options.Length(address => address.Street, 1, 100))
+                        .With<Address>(options => options.Length(address => address.City, 1, 100))
+                        .With<Address>(options => options.Length(address => address.Country, 1, 100));
+            }
         }
 
         private static DomainGenerator EmployeeGenerator(ISession session)
         {
-            return WithAddress(new DomainGenerator())
+            return Using.TheseConventions()
                 .With<Employee>(options => options.Ignore(employee => employee.Id))
-                .With<Employee>(options => options.For(employee => employee.FirstName, new StringGenerator(1, 50)))
-                .With<Employee>(options => options.For(employee => employee.LastName, new StringGenerator(1, 75)))
-                .With<Employee>(options => options.For(employee => employee.Title, new StringGenerator(1, 50)))
+                .With<Employee>(options => options.Length(employee => employee.FirstName, 1, 50))
+                .With<Employee>(options => options.Length(employee => employee.LastName, 1, 75))
+                .With<Employee>(options => options.Length(employee => employee.Title, 1, 50))
                 .With<Employee>(options => options.For(employee => employee.Salary, new DecimalGenerator(1700, 3500)))
-                .With<Employee>(options => options.For(employee => employee.Phone, new StringGenerator(1, 15)));
+                .With<Employee>(options => options.Length(employee => employee.Phone, 1, 15))
+                .ForEach<Employee>(employee => session.Save(employee));
         }
 
         public static void Create(ISession session)
         {
-            var customers = WithAddress(new DomainGenerator())
-                .With<Customer>(options => options.Ignore(customer => customer.Id))
-                .With<Customer>(options => options.For(customer => customer.Name, new StringGenerator(5, 100)))
+            var customers = Using.TheseConventions()
+                .With<Customer>(options => options.Length(customer => customer.Name, 5, 100))
                 .With<Customer>(options => options.For(customer => customer.DiscountPercentage, new DoubleGenerator(0, 25)))
                 .ForEach<Customer>(customer => session.Save(customer))
                 .Many<Customer>(20, 40)
                 .ToArray();
 
             var managers = EmployeeGenerator(session)
-                .ForEach<Employee>(employee => session.Save(employee))
                 .Many<Employee>(2);
 
             var employees = EmployeeGenerator(session)
                 .ForEach<Employee>(employee => Maybe.Do(() => managers.PickOne().AddSubordinate(employee)))
-                .ForEach<Employee>(employee => session.Save(employee))
                 .Many<Employee>(20)
                 .ToArray();
 
-            var suppliers = WithAddress(new DomainGenerator())
-                .With<Supplier>(options => options.Ignore(supplier => supplier.Id))
-                .With<Supplier>(options => options.For(supplier => supplier.Website, new StringGenerator(1, 100)))
+            var suppliers = Using.TheseConventions()
+                .With<Supplier>(options => options.Length(supplier => supplier.Website, 1, 100))
                 .Many<Supplier>(20)
                 .ToArray();
 
-            var products = new DomainGenerator()
-                .With<ProductSource>(options => options.Ignore(productsource => productsource.Id))
+            var products = Using.TheseConventions()
                 .ForEach<ProductSource>(productsource => session.Save(productsource))
-                .With<Product>(options => options.Ignore(product => product.Id))
                 .With<Product>(options => options.Ignore(product => product.Version))
                 .With<Product>(options => options.For(
                     product => product.Category,
@@ -68,15 +70,13 @@ namespace NHibernateWorkshop
                     ProductCategory.DairyProducts,
                     ProductCategory.Produce))
                 .With<Product>(g => g.Method<double>(0, 5, (product, d) => product.AddSource(suppliers.PickOne(), d)))
-                .With<Product>(options => options.For(product => product.Name, new StringGenerator(1, 50)))
+                .With<Product>(options => options.Length(product => product.Name, 1, 50))
                 .ForEach<Product>(product => session.Save(product))
                 .Many<Product>(50)
                 .ToArray();
 
-            WithAddress(new DomainGenerator())
-                .With<OrderItem>(options => options.Ignore(item => item.Id))
+            Using.TheseConventions()
                 .With<OrderItem>(options => options.For(item => item.Product, products))
-                .With<Order>(options => options.Ignore(order => order.Id))
                 .OneToMany<Order, OrderItem>(1, 5, (order, item) => order.AddItem(item))
                 .With<Order>(options => options.For(order => order.Customer, customers))
                 .With<Order>(options => options.For(order => order.Employee, employees))
